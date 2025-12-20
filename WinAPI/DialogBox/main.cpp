70e8fc83609287dd209e38f7688936f88507981f
User <User@DESKTOP-QHG18FL>
2025-12-20 22:01:25 +0300

    Messages

--- WinAPI/DialogBox/main.cpp
+++ WinAPI/DialogBox/main.cpp
@@ -2,10 +2,24 @@
 #include"resource.h"
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+/*
+--------------------------------------------------------------------------
+Окно, сообщение и параметры сообщения.
+hwnd - окно;
+uMsg - сообщение;
+wParam, lParam - параметры сообщения;
+----------------
+HWND - Handler to Window (Дескриптор, описатель окна);
+WPARAM и LPARAM - это самые обычные значения типа DWORD (INT).
+Эти параметры часто делят на HIWORD и LOWORD
+0 kb
+--------------------------------------------------------------------------
+*/
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	"Button", "Edit";
 	return 0;
 }
 
@@ -14,10 +28,29 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:	//Инициализация окна диалога. Эта секция отрабатывает 1 раз - при запуске окна.
-		break;
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+	}
+	break;
 	case WM_COMMAND:	//В этой секции обраьатываеются нажатия кнопок, клавиш и другие события
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			//Функция GetDlgItem() позволяет получить HWND элемента окна 'hwnd', 
+			//по RESOURCE_ID нужного элемента.
+			//RESOURCE_ID - это самое обычное значение типа 'INT'
+			//----------------------------------------
+			//WM_- WindowMessage (такое сообщение можно отправить любому окну)
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
 		case IDOK:		MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL:	EndDialog(hwnd, 0); break;
 		}
