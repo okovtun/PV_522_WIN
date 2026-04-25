000f793f5e89f0c88f5069b999c4011a0324e10a
User <User@DESKTOP-QHG18FL>
2026-04-25 19:30:36 +0300

    Binding

---
+++ WPF/Binding/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.ComponentModel;
+
+namespace Binding
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		string boundText;
+		public string BoundText
+		{
+			get => boundText;
+			set
+			{
+				boundText = value;
+				OnPropertyChanged();
+			}
+		}
+		public MainWindow()
+		{
+			DataContext = this;
+			InitializeComponent();
+			txtInput.Focus();
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+		void OnPropertyChanged()
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+		}
+
+		private void BtnDefault_Click(object sender, RoutedEventArgs e)
+		{
+			BoundText = "Default";
+		}
+	}
+}
