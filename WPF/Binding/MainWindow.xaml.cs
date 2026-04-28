41bc5c595f6ff955d742ce950328f8a3750d5a66
User <User@DESKTOP-QHG18FL>
2026-04-28 19:01:59 +0300

    CallerMemberName

--- WPF/Binding/MainWindow.xaml.cs
+++ WPF/Binding/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Binding
 {
@@ -30,6 +31,7 @@ namespace Binding
 			{
 				boundText = value;
 				OnPropertyChanged();
+				//OnPropertyChanged(nameof(BoundText));
 			}
 		}
 		public MainWindow()
@@ -39,9 +41,9 @@ namespace Binding
 			txtInput.Focus();
 		}
 		public event PropertyChangedEventHandler PropertyChanged;
-		void OnPropertyChanged()
+		void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 
 		private void BtnDefault_Click(object sender, RoutedEventArgs e)
